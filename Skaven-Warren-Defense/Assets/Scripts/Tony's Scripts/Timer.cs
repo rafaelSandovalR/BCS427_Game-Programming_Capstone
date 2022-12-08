53414112b65bb8c6dd966ac4c32545a3b7497fdb
Alrick96 <112648005+Alrick96@users.noreply.github.com>
2022-12-08 10:14:23 -0500

    model Orc

--- Skaven-Warren-Defense/Assets/Scripts/Tony's Scripts/Timer.cs
+++ Skaven-Warren-Defense/Assets/Scripts/Tony's Scripts/Timer.cs
@@ -10,24 +10,22 @@ public class Timer : MonoBehaviour
     private float currentTime;
     public bool gameIsOver = false;
     public Text mainTimerDisplay;
-    // Start is called before the first frame update
+    
     void Start()
     {
         currentTime = startTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (!gameIsOver)
         {
             if (currentTime < 0)
-            { // check to see if timer has run out
-              // EndGame();
+            { 
                 print("endgame");
             }
             else
-            { // game playing state, so update the timer
+            {
                 currentTime -= Time.deltaTime;
                 if (mainTimerDisplay)
                     mainTimerDisplay.text = currentTime.ToString("0.00");
